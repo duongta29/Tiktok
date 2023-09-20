e073892925153b2e7e4ff5e0990eb459fa5d29c9
ice29s <duong.ta290601@gmail.com>
2023-09-20 17:05:53 +0700

    modified:   post_model.py

--- post_model.py
+++ post_model.py
@@ -12,23 +12,23 @@ class Post:
         self.avatar = ""
         self.created_time = ""
         self.content = ""
-        self.image_url = []
+        # self.image_url = []
         self.like = 0
         self.comment = 0
-        self.haha = 0
-        self.wow = 0
-        self.sad = 0
+        # self.haha = 0
+        # self.wow = 0
+        # self.sad = 0
         self.love = 0
-        self.angry = 0
+        # self.angry = 0
         self.share = 0
         self.domain = ""
         self.hastag = []
         self.music = ""
-        self.title = ""
+        # self.title = ""
         self.duration = 0
         self.view = 0
-        self.description = ""
-        self.video = ""
+        # self.description = ""
+        # self.video = ""
         self.source_id = ""
         
     #Hàm xác định xem post được crawl đủ hay chưa
